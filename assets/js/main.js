85131445383a6147bdb4021c2c57b7cd6beb46ac
Hugo Zhou <zhouhu@hotmail.com>
2020-07-03 19:47:35 +1200

    init home and 404 page

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+(function () {
+  $(function () {
+    var n, e, t;
+    return n = function () {
+      return $("h1").append("<span>"), $("h1 span").css({
+        display: "block",
+        width: "0px",
+        height: "26px",
+        background: "#fff",
+        "margin-top": "21px"
+      }).animate({width: t + "px"}, 800), setTimeout(function () {
+        return $("body").removeClass("loading")
+      }, 1e3)
+    }, -1 !== navigator.appVersion.indexOf("Windows") && $("body").addClass("pcuser"), e = $("html").attr("lang"), "zh" === e ? (t = 439, "en" === navigator.language.substring(0, 2)) : t = 264, $(".subjects_top li, .courses li > a:first-of-type").on("mouseenter mouseleave", function (n) {
+      return "mouseenter" === n.type ? $("body").addClass("hover") : $("body").removeClass("hover")
+    }), $(window).on("load", function () {
+      return window.innerWidth > 620 ? n() : $("body").removeClass("loading")
+    })
+  }), $(window).resize(function () {
+    return $(window).width() <= 620 ? ($("h2").hide(), $("h1 span").hide(), $(".courses li:first-child, .subjects, #anim").hide()) : ($("h2").show(), $("h1 span").show(), $(".courses li:first-child, .subjects, #anim").show()), $(".subjects_top").find("li").on("mouseenter", function (n) {
+      var e;
+      e = $(this).find("a").text(), dataLayer.push({
+        event: "navigation",
+        eventCategory: "Company",
+        eventAction: "Hover",
+        eventLabel: e
+      })
+    }), $("a").on("click", function () {
+      var n, e, t;
+      t = $(this), e = t.text(), n = t.attr("href"), dataLayer.push({
+        event: "navigation",
+        eventCategory: "Links",
+        eventAction: "Click",
+        eventLabel: e,
+        eventValue: n
+      })
+    })
+  })
+}).call(this);
